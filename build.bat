50073baa93422d741192cb073ff127bab394b309
kqkqku86-a11y <kqkqku86@gmail.com>
2026-06-04 13:25:14 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,11 +7,11 @@ set ANDROID_TOOLCHAIN="D:\Tools\AndroidSDK\ndk\21.3.6528147\build\cmake\android.
 
 mkdir Build && cd Build
 	
-FOR %%i in %TARGETS% do (
+FOR %%i IN (%TARGETS%) DO (
     mkdir %%i
 	cd %%i
 
-    cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%ANDROID_TOOLCHAIN% -DANDROID_PLATFORM=21 -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=%%i ..
+    cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%ANDROID_TOOLCHAIN% -DANDROID_PLATFORM=21 -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=%%i ../..
 	cmake --build . -j4
     echo Current dir:
     cd
